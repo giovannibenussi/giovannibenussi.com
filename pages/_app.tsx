19d5cfe4913a675633527986e6653f49dfe9597f
Giovanni Benussi <giovanni.benussi@usach.cl>
2022-10-14 08:25:21 -0300

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+
+      <Script id="analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+          page_path: window.location.pathname,
+          });
+      `}
+      </Script>
       <Component {...pageProps} />
     </ChakraProvider>
   )
