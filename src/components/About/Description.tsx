149de2f4901eea924ecdb5a41d8edf0636f8269e
Giovanni Benussi <giovanni.benussi@usach.cl>
2023-08-30 09:27:36 -0400

    Update about page

--- src/components/About/Description.tsx
+++ src/components/About/Description.tsx
@@ -1,19 +1,28 @@
 export function Description() {
   return (
-    <div>
+    <div className="mb-20">
       <p>
-        I&apos;m a fullstack engineer working at{' '}
+        I'm a software developer that really loves his job. I've been working on
+        the web since Dreamweaver was a thing (I feel kinda old writing this
+        😅).
+      </p>
+
+      <p>
+        I have a master&apos;s in Computer Science and I work as developer at{' '}
         <a href="https://www.propertysimple.com/">PropertySimple</a>.
       </p>
+
       <p>
-        I have a master in Computer Science and I&apos;ve worked 10 years across
-        the whole stack.
+        Nowadays, I work primarily with React and the whole JavaScript
+        ecosystem.
       </p>
+
       <p>Current Stack:</p>
       <ul>
         <li>Node.js</li>
         <li>React</li>
         <li>TypeScript</li>
+        <li>Remix</li>
         <li>Next.js</li>
         <li>Cloud with AWS and Cloudflare</li>
       </ul>
@@ -25,6 +34,21 @@ export function Description() {
         <li>Redis</li>
         <li>Server Management</li>
       </ul>
+
+      <p>
+        This year, I speaked at{' '}
+        <a href="https://remix.run/conf/2023">RemixConf</a>:
+      </p>
+
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/TR_wn2y-SYQ?si=rZrqqysA-4zRHqjB"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
     </div>
   )
 }
