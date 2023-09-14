cff4148f41603b561947130b3cbae909f9049691
Giovanni Benussi <giovanni.benussi@usach.cl>
2023-09-14 09:02:02 -0300

    Disable Sentry on development

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,14 +3,16 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
-Sentry.init({
-  dsn: "https://43d100c8834532a9181bf49fcb828a08@o4505799392165888.ingest.sentry.io/4505799394525184",
+if (process.env.NODE_ENV !== 'development') {
+  Sentry.init({
+    dsn: 'https://43d100c8834532a9181bf49fcb828a08@o4505799392165888.ingest.sentry.io/4505799394525184',
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  })
+}
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,6 +4,7 @@
 
 import * as Sentry from '@sentry/nextjs'
 
+if (process.env.NODE_ENV !== 'development') {
 Sentry.init({
   dsn: 'https://43d100c8834532a9181bf49fcb828a08@o4505799392165888.ingest.sentry.io/4505799394525184',
 
@@ -13,3 +14,4 @@ Sentry.init({
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 })
+}
